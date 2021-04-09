b7dc7e8cc4244d2b409b0a06a75fb441e45f8b5a
evekogan <eve.kogan22@sitechhs.com>
2021-04-08 23:38:39 -0400

    done i hope

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,25 @@
 console.log("connected");
 
+const submitButton = document.querySelector("#submit-button");
+
 function submitAnswers() {
   const total = 10; //max number of questions
   let score = 0; //start score at 0
   //save results from the form as variables
-  const q1 = document.forms["quizForm"]["q1"].value;
-  const q2 = document.forms["quizForm"]["q2"].value;
-  const q3 = document.forms["quizForm"]["q3"].value;
-  const q4 = document.forms["quizForm"]["q4"].value;
-  const q5 = document.forms["quizForm"]["q5"].value;
-  const q6 = document.forms["quizForm"]["q6"].value;
-  const q7 = document.forms["quizForm"]["q7"].value;
-  const q8 = document.forms["quizForm"]["q8"].value;
-  const q9 = document.forms["quizForm"]["q9"].value;
-  const q10 = document.forms["quizForm"]["q10"].value;
+  const q1 = document.forms["stardewQuiz"]["q1"].value;
+  const q2 = document.forms["stardewQuiz"]["q2"].value;
+  const q3 = document.forms["stardewQuiz"]["q3"].value;
+  const q4 = document.forms["stardewQuiz"]["q4"].value;
+  const q5 = document.forms["stardewQuiz"]["q5"].value;
+  const q6 = document.forms["stardewQuiz"]["q6"].value;
+  const q7 = document.forms["stardewQuiz"]["q7"].value;
+  const q8 = document.forms["stardewQuiz"]["q8"].value;
+  const q9 = document.forms["stardewQuiz"]["q9"].value;
+  const q10 = document.forms["stardewQuiz"]["q10"].value;
   //check to see if user forgot any questions
-  for (const i = 1; i <= total; i++) {
+  for (let i = 1; i <= total; i++) {
     if (eval("q" + i) == null || eval("q" + i) == "") {
       alert("You missed question " + i);
-      return false; //prevents page refresh
     }
   }
 
@@ -26,15 +27,38 @@ function submitAnswers() {
   const answers = ["b", "b", "c", "c", "a", "d", "d", "a", "b", "a"];
 
   //check answers with for loop
-  for (const i = 1; i <= total; i++) {
+  for (let i = 1; i <= total; i++) {
     if (eval("q" + i) == answers[i - 1]) {
       score++; //add score for every question right
     }
   }
   console.log("You scored " + score + " out of " + total); //temporary console log
-
   //display results
   const results = document.getElementById("results");
-  results.innerText = `You scored ${score} out of ${max}.`;
-  return false;
+  const reaction = document.getElementById("reaction");
+  results.innerText = `You scored ${score} out of 10.`;
+  if (score === 10) {
+    reaction.innerText =
+      "Congratulations! You got a perfect score! Are you sure you aren't ConcernedApe?";
+  }
+  if (score >= 7 && score < 10) {
+    reaction.innerText = "Nice job! You know a lot about the game";
+  }
+  if (score >= 4 && score < 7) {
+    reaction.innerText =
+      "Could be better. You should brush up on your Stardew knowledge!";
+  }
+  if (score >= 1 && score < 4) {
+    reaction.innerText =
+      "Do you even play the game? I'll answer that for you. Clearly not.";
+  }
+  if (score === 0) {
+    reaction.innerText =
+      "I'm disapointed. Genuinely. Not even JojaMart deserves you.";
+  }
 }
+
+submitButton.addEventListener("click", function (e) {
+  submitAnswers();
+  e.preventDefault();
+});
